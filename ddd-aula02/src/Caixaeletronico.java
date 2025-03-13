b3915b41d1b5698805da833a06578462ef477242
gbalbano <bulletgaba066@gmail.com>
2025-03-13 12:17:15 -0300

    texto

---
+++ ddd-aula02/src/Caixaeletronico.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Caixaeletronico {
+    public static void main(String[] args)
+       Scanner leitor = new Scanner(System.in);
+       int valor, notas50, notas20, notas10;
+
+
+
+         }
+
